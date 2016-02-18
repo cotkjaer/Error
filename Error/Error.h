0edef9b06d16fb23e545660887ffe97bc1efed26
christian otkjær <christian.otkjaer@gmail.com>
2016-02-18 17:47:46 +0100

    Initial Commit

---
+++ Error/Error.h
@@ -0,0 +1,19 @@
+//
+//  Error.h
+//  Error
+//
+//  Created by Christian Otkjær on 18/02/16.
+//  Copyright © 2016 Christian Otkjær. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Error.
+FOUNDATION_EXPORT double ErrorVersionNumber;
+
+//! Project version string for Error.
+FOUNDATION_EXPORT const unsigned char ErrorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Error/PublicHeader.h>
+
+
